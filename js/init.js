9194d3704c6bf3b953c00bcdb0b66ab15317b0c1
Ryan Hibbert <rhibbert@wayfair.com>
2020-04-19 23:49:58 +0200

    Added Dynamic quote + author + username to the new tab screen

--- js/init.js
+++ js/init.js
@@ -1,3 +1,19 @@
+// Copyright 2020 Stik Wid It Authors. All rights reserved.
 
-// const resources = require('./modules/web-resources');
+'use strict';
+
+let quote = document.getElementById('quote');
+chrome.storage.sync.get('quote', function(data) {
+  quote.innerHTML = data.quote;
+});
+
+const author = document.getElementById('author');
+chrome.storage.sync.get('author', function(data) {
+  author.innerHTML = data.author;
+});
+
+const author = document.getElementById('user-greeting');
+chrome.storage.sync.get('user-name', function(data) {
+    author.innerHTML =  data.username ? `Hey ${data.username}` : "Hey You";
+});
 
